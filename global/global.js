05a33e3a2702a02c2683ce3c4cc1aaf5ba3db322
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-08-14 14:05:19 -0300

    ajustes

--- global/global.js
+++ global/global.js
@@ -1,7 +1,6 @@
 (function() {
-  var script = document.createElement('script');
-  script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
-  script.integrity = "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz";
+  let script = document.createElement('script');
+  script.src = " 	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js";
   script.crossOrigin = "anonymous";
   document.head.appendChild(script);
 })();
